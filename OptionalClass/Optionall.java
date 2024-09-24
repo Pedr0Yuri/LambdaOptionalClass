c46df0e879ade305e54bcf3dbe762890507d2efa
Pedro Yuri de Oliveira Góes <165570331+Pedr0Yuri@users.noreply.github.com>
2024-09-24 15:31:14 -0300

    Update Optionall.java

--- OptionalClass/Optionall.java
+++ OptionalClass/Optionall.java
@@ -6,8 +6,8 @@ public class Optionall {
         //Verifica se é nulo, então este método retorna se é vazio ou não.
                                                     
         if(checkNull.isPresent()){  // Checa se o valor vazio da classe optinal esta presente 
-            String lowercaseString = str[5].toLowerCase();  
-            System.out.print(lowercaseString);  
+            String stringAula = str[5].toLowerCase();  
+            System.out.print(stringAula);  
         }else  
             System.out.println("Não há valor de String definido");  
     }  
