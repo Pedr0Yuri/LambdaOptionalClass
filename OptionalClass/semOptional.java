fa9fda6dbe034e7d78b91d74c0fbaffbd369f218
Pedro Yuri de Oliveira Góes <165570331+Pedr0Yuri@users.noreply.github.com>
2024-09-24 15:29:49 -0300

    Add files via upload

---
+++ OptionalClass/semOptional.java
@@ -0,0 +1,8 @@
+public class semOptional {  
+    public static void main(String[] args) {  
+        String[] str = new String[10];  
+            String stringAula = str[5].toLowerCase();  
+            System.out.print(stringAula);  
+            System.out.println("Não há valor de String definido");  
+    }  
+}  
